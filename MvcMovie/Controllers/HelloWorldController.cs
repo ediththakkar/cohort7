eb02bf8da6b9166c49eab23b27ac97e5d15bbdc9
Deep Dive Student <Deep Dive Student@LAPTOP-48OTEKRR>
2018-08-12 09:27:30 -0600

    initial check is completed

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld
+        //public string Index()
+        //{
+        //    return "This is my default action";
+        //}
+
+        public IActionResult index()
+        {
+            return View();
+        }    
+
+        //Get: /Welcome/
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+            ViewData["Messages"] = $"Hello {name}";
+            ViewData["NumTimes"] = ID;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int  ID { get; set; }
+
+        [StringLength(60, MinimumLength =1)]
+        [Required]
+
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Text, ErrorMessage = "Please enter a valid date")]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+
+        [StringLength(30)]
+        [Required]
+        [RegularExpression(@"[A-Z]+[a-zA-z""'\s-]*$", ErrorMessage = "Genre must start with a capital letter and only contain letters, single quotes and dashes")]
+        public string Genre { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
+        [DataType(DataType.Currency)]
+        [Range(1,100, ErrorMessage ="Please enter a valid price between $1 and !00")]
+        [Required(ErrorMessage = "Please enter a valid price")]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        [Required]
+        [RegularExpression(@"[A-Z]+[a-zA-z""'\s-]*$")]
+        public string Rating { get; set; }
+
+
+    }
+}
